5775076ad184360ae2a377292c5acb3cd4045e34
sushiAlii <aliasgar.laut@gmail.com>
2021-12-08 13:00:55 +0800

    copying css from buildpsace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MainContent } from "./components/MainContent.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { useState } from "react";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar
+        currentAccount={currentAccount}
+        setCurrentAccount={setCurrentAccount}
+      />
+      <MainContent />
     </div>
   );
 }
